4408ef199885e8a618b0a125e07ad92ff01afe87
Yogesh <yogesh.mopada@ishafoundation.org>
2023-04-01 15:09:59 +0530

    added comments for code

--- main.py
+++ main.py
@@ -8,41 +8,47 @@ app = Flask(__name__)
 
 # Home page of the App
 
-
+# This is the home page of the app where the user can choose their input method
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
+# This is the page of the app where the user can get data of an area by selecting the area on the map
 @app.route("/map")
 def map():
     return render_template('map.html')
 
+# This is the page of the app where the user can input coordinates and get data at that coordinates
 @app.route("/coordinates")
 def coordinates():
     return render_template('coordinates.html')
 
-@app.route("/coordinates-list")
-def coordinateslist():
-    return render_template('list.html')
+# This is the page of the app where the user can input diagonal coordinates and get data at that coordinates
+@app.route("/diagonal-coordinates")
+def diagonalcoordinates():
+    return render_template('diagonal-coordinates.html')
 
+
+# This function takes in user input(boundary coordinates) and returns a JSON object with the calculated data
 
 @app.route("/calculate", methods=['POST'])
 def calculate():
-    initialise()
+    initialise() # Initialize the Earth Engine API
     output = request.get_json()
     print("Request arg type: ", type(output))
     print(output)
-    result = json.loads(output)
+    result = json.loads(output) # Load the JSON input into a Python object
     print("JSON loaded input: ", type(result))
     print("After JSON loads: ", result)
     print(result[0])
-    region = ee.Geometry.Polygon(result)
-    ans = given_data(region)
+    region = ee.Geometry.Polygon(result) # Create a region object from the input coordinates
+    ans = given_data(region) # Calculate the data for the region using the Earth Engine API
     # print("In calculate method :")
     return jsonify(ans)  # Return a JSON object instead of a JSON string
 
 
+# This route takes the latitude and longitude values from the form and outputs the values at that location
 @app.route("/result", methods=['POST'])
 def ans():
     print('submit button clicked')
