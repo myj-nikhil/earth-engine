f125676d9c3d4c15e342271a882715b8fa7502ed
Yogesh <yogesh.mopada@ishafoundation.org>
2023-04-20 14:44:10 +0530

    used ajaxPost function to reduce lines of code

--- static/googleMap.js
+++ static/googleMap.js
@@ -149,85 +149,8 @@ function myfunction() {
     let turfPolygon = turf.polygon([coordinates]);
     let area = turf.area(turfPolygon);
     console.log(`Area of the polygon is ${area}`)
-    const rounded_area = Math.round(area * 100) / 100;
-    console.log("Type of request to Python server is ", typeof s)
-    console.log("JSON Stringified input:", s);
-    targetArea.innerHTML = `<p>Calculating...</p>`
-    $.ajax({
-        url: "/calculate",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(s),
-        success: function (response) {
-            console.log("Type of response from server is: ", typeof response)
-            console.log("Response from the server", response); // Log the response from the server
-            // const data = response 
-            // Parse the response string to a JSON object
-            console.log(response); // Log the parsed JSON object
-            /// Inside the AJAX success callback
-
-            const table = document.createElement('table');
-
-            // Create header row
-            const headerRow = document.createElement('tr');
-            const header1 = document.createElement('th');
-            header1.innerText = 'Key';
-            const header2 = document.createElement('th');
-            header2.innerText = 'Value';
-            headerRow.appendChild(header1);
-            headerRow.appendChild(header2);
-            table.appendChild(headerRow);
-
-            // Create rows for each key-value pair in the response object
-            for (const [key, value] of Object.entries(response)) {
-                const row = document.createElement('tr');
-                const cell1 = document.createElement('td');
-                cell1.innerText = key;
-                const cell2 = document.createElement('td');
-                if (typeof value === 'object') {
-                    // If the value is an object, create a sub-table
-                    const subTable = document.createElement('table');
-                    for (const [subKey, subValue] of Object.entries(value)) {
-                        const subRow = document.createElement('tr');
-                        const subCell1 = document.createElement('td');
-                        subCell1.innerText = subKey;
-                        const subCell2 = document.createElement('td');
-                        subCell2.innerText = subValue;
-                        subRow.appendChild(subCell1);
-                        subRow.appendChild(subCell2);
-                        subTable.appendChild(subRow);
-                    }
-                    cell2.appendChild(subTable);
-                } else {
-                    // If the value is not an object, just display it in a cell
-                    cell2.innerText = value;
-                }
-                row.appendChild(cell1);
-                row.appendChild(cell2);
-                table.appendChild(row);
-            }
-            const row  = document.createElement('tr');
-            const areaKeyCell  = document.createElement('td');
-            const areaValueCell = document.createElement('td');
-            areaKeyCell.innerText = 'Area (sq m.)'
-            areaValueCell.innerText = rounded_area
-            row.appendChild(areaKeyCell)
-            row.appendChild(areaValueCell)
-            table.appendChild(row) 
-            // Replace the contents of the ans div with the table
-            const targetArea = document.getElementById("ans");
-            targetArea.innerHTML = '';
-            targetArea.appendChild(table);
-
-        },
-
-        error: function (xhr, status, error) {
-            console.log(error); // Log any errors
-            const targetArea = document.getElementById("ans");
-            targetArea.innerHTML = `<p>Please select a smaller area</p>`
-
-        }
-    });
+    const roundedArea = Math.round(area * 100) / 100;
+    ajaxPost(coordinates,targetArea,true,roundedArea);
 }
 else {
     targetArea.innerHTML = 'Please select atleast 3 points'
