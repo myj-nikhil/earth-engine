25eed1d7cd2f8aa7d64ac5a84aec69c324f8bc4a
Yogesh <yogesh.mopada@ishafoundation.org>
2023-05-02 13:28:30 +0530

    commented lines of second button(give soil data) button functionality

--- static/diagCoord.js
+++ static/diagCoord.js
@@ -1,23 +1,22 @@
  // get the form element
- const form = document.querySelector("form");
 
  // add an event listener to the form for when it is submitted
- form.addEventListener("submit", function (event) {
+ document.getElementById("submit").addEventListener("click", () => {
    // prevent the default form submission behavior
-   event.preventDefault();
+   
 
    // get the values of the form inputs
    const topLeftLat = parseFloat(
-     document.querySelector("#top-left-lat").value
+     document.getElementById("top-left-lat").value
    );
    const topLeftLong = parseFloat(
-     document.querySelector("#top-left-long").value
+     document.getElementById("top-left-long").value
    );
    const bottomRightLat = parseFloat(
-     document.querySelector("#bottom-right-lat").value
+     document.getElementById("bottom-right-lat").value
    );
    const bottomRightLong = parseFloat(
-     document.querySelector("#bottom-right-long").value
+     document.getElementById("bottom-right-long").value
    );
 
    // validate the input values
@@ -52,3 +51,54 @@
    const roundedArea = Math.round(area * 100) / 100;
   ajaxPost(coordinates,targetArea,true,roundedArea);
  });
+
+//  document.getElementById("submit2").addEventListener("click", () => {
+//   // prevent the default form submission behavior
+  
+
+//   // get the values of the form inputs
+//   const topLeftLat = parseFloat(
+//     document.getElementById("top-left-lat").value
+//   );
+//   const topLeftLong = parseFloat(
+//     document.getElementById("top-left-long").value
+//   );
+//   const bottomRightLat = parseFloat(
+//     document.getElementById("bottom-right-lat").value
+//   );
+//   const bottomRightLong = parseFloat(
+//     document.getElementById("bottom-right-long").value
+//   );
+
+//   // validate the input values
+//   if (
+//     isNaN(topLeftLat) ||
+//     isNaN(topLeftLong) ||
+//     isNaN(bottomRightLat) ||
+//     isNaN(bottomRightLong)
+//   ) {
+//     alert("Please enter valid coordinates.");
+//     return;
+//   }
+
+//   // calculate the other two corner coordinates
+//   const topRightLat = topLeftLat;
+//   const topRightLong = bottomRightLong;
+//   const bottomLeftLat = bottomRightLat;
+//   const bottomLeftLong = topLeftLong;
+
+//   // create an array of the corner coordinates in clockwise order
+//   const coordinates = [
+//     [topLeftLong, topLeftLat],
+//     [topRightLong, topRightLat],
+//     [bottomRightLong, bottomRightLat],
+//     [bottomLeftLong, bottomLeftLat],
+//     [topLeftLong, topLeftLat],
+//   ];
+//   targetArea = document.getElementById("ans");
+//   let turfPolygon = turf.polygon([coordinates]);
+//   let area = turf.area(turfPolygon);
+//   console.log(`Area of the polygon is ${area}`)
+//   const roundedArea = Math.round(area * 100) / 100;
+//  secondajaxPost(coordinates,targetArea,true,roundedArea);
+// });
